4edb79d40cb72d9027832c467c6997a51d9c8fd3
JeanFrancoLeonB/wdd131 <>
2024-11-08 23:05:29 -0500

    yes

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,28 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter Number for X: ");
-        string valueforx = Console.ReadLine();
-        Console.Write("Enter Number for Y: ");
-        string valuefory = Console.ReadLine();
-        int x = int.Parse(valueforx);
+        Console.Write("PLease enter your grade: ");
 
-        int y = int.Parse(valuefory);
+        string grade = Console.ReadLine();
 
-        if (x>y)
-        {
-        Console.WriteLine("Greater");
+        int x = int.Parse(grade);
+        string letter = "";
+
+        if (x>= 90){
+            letter = "A";
         }
-            else if (x<y)
-            {
-            Console.WriteLine("Less");
-            }
-            else
-            {
-                Console.WriteLine("equal");
-            }
-        
+        else if (x>= 80) {
+            letter = "B";
+        }
+           else if (x>= 70) {
+            letter = "C";
+        }
+           else if (x>= 60) {
+            letter = "D";
+        }
+            else {
+            letter = "F";
+        }
 
+            Console.WriteLine($"Your grade is: {letter}");
+
+        if (x >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course !");
+        }
+         else{
+            Console.WriteLine("You Failed the course, You can try again, you can do it !");
+        }
     }
        
 }
