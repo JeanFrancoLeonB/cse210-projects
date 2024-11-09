c1a013d878e793de0b3e9a94922b2a474d48061d
JeanFrancoLeonB/wdd131 <>
2024-11-08 23:42:27 -0500

    save

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       Console.Write("Select a number between 1 and 100, inclusive: ");
+       string response = Console.ReadLine();
+       Console.Write("Are you sure you have a number?: ");
+       response = Console.ReadLine();
+       if (response == "y");
+       Console.WriteLine(" I will try to guess your number");
     }
 }
