96903521eaea192cff643e8cd431f9bd256c02b4
JeanFrancoLeonB <jeanfra08@hotmail.com>
2025-03-03 00:21:10 -0500

    Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        
+
+        string first_name;
+
+        Console.Write("What is your first name? ");   /*Writeline hace que el input pase a la siguiente linea, write en la misma*/
+        first_name = Console.ReadLine();
+
+
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+
     }
 }
