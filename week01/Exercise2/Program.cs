a7bbc6b9bcd76810870119dda2bd7406a74862e6
JeanFrancoLeonB <jeanfra08@hotmail.com>
2025-03-03 23:43:51 -0500

    etext3

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        /*    &&  And Operator  !() Not Operator   */
+
+        Console.Write("Enter your Grade please: ");
+        string strnumbergrade = Console.ReadLine();
+        int numbergrade = int.Parse(strnumbergrade);
+
+
+        string grade;
+
+        if (numbergrade >= 90)
+        {
+        grade = "A";
+        }
+        else if (numbergrade >= 80)
+        {
+        grade = "B";
+        }
+        else if (numbergrade >= 70)
+        {
+        grade = "C";
+        }
+        else if (numbergrade >= 60)
+        {
+        grade = "D";
+        }
+        else
+        {
+        grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is {grade}");
+
+        if (grade ==  "A"  || grade ==  "B" || grade ==  "C" )
+        Console.WriteLine($"Congrats, you pass the class !!");
+        else
+        Console.WriteLine($"Sorry, you didn't pass the class");
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,57 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        /*string response = "yes";
+
+        while (response == "yes")
+        {
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+        } */
+
+
+
+        /* El Do While ejecuta */
+
+        /*
+        string response;
+        do
+        {
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+        } while (response == "yes");
+        */
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        string answer;
+
+
+        Console.WriteLine($"The number the computer choose is {number}");
+        int guessnumber = -1;
+        /*El valor de -1 se agrega para que el while funcione para que pueda comparar
+        el numero aleaorio de la computadora con  un valor que no esta dentro
+         del rango al inicializar para que la condición que la establecemos sea falsa y
+          asi se ejecute el bucle */
+        while (number != guessnumber)
+        {
+            Console.Write("Enter your number please: ");
+            string strguessnumber = Console.ReadLine();
+            guessnumber = int.Parse(strguessnumber);
+        
+            if (number > guessnumber)
+            {
+                answer = "Higher";
+            }
+            else if (number < guessnumber)
+            {
+                answer = "Lower";
+            }
+            else
+            {
+                answer = "You guessed it!";
+            }
+             Console.WriteLine($"{answer}");
+        }
     }
 }
