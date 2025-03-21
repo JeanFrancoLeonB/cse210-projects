20364633ee04f16e8908a96a35aa7950ba171799
JeanFrancoLeonB <jeanfra08@hotmail.com>
2025-03-21 00:53:18 -0400

    we3

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,25 @@
 using System;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction p1 = new Fraction(); // Don't pass any information to get the default values
+        Console.WriteLine(p1.GetFractionString());
+        Console.WriteLine(p1.GetDecimalValue());
+
+        Fraction p2 = new Fraction(5);  // pass top
+        Console.WriteLine(p2.GetFractionString());
+        Console.WriteLine(p2.GetDecimalValue());
+
+        Fraction p3 = new Fraction(3, 4); // pass the top and th bottom
+        Console.WriteLine(p3.GetFractionString());
+        Console.WriteLine(p3.GetDecimalValue());
+
+        Fraction p4 = new Fraction(1, 3);
+        Console.WriteLine(p4.GetFractionString());
+        Console.WriteLine(p4.GetDecimalValue());
     }
 }
